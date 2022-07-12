53b8b2a2e20bdc428ff2888e6116e672a5c3a404
Tox1k <sashasolud@gmail.com>
2022-07-12 16:16:44 +0200

    added comments

--- server/config.py
+++ server/config.py
@@ -1,9 +1,12 @@
 class Config(object):
+    # WEB APPLICATION PASSWORD
     WEB_PASSWORD = 'password'
+
+    # CLIENT API TOKEN
     API_TOKEN = 'token'
 
-    YOUR_TEAM = '10.60.27.1'
-    TEAM_TOKEN = '8a8d25f465bec01d'
+    YOUR_TEAM = '10.60.14.1'
+    TEAM_TOKEN = '' # team token for flag submission
     TEAMS = ['10.60.{}.1'.format(i) for i in range(1, 35)]
     TEAMS.remove(YOUR_TEAM)
 
@@ -11,12 +14,13 @@ class Config(object):
     FLAG_ALIVE = 5 * ROUND_DURATION
     FLAG_FORMAT = r'[A-Z0-9]{31}='
 
-    FLAGID_URL = "https://6.enowars.com/scoreboard/attack.json"
+    
+    FLAGID_URL = '' # flag_ids endpoint, leave blank if none
 
-    SUB_LIMIT = 1
-    SUB_INTERVAL = 5
-    SUB_PAYLOAD_SIZE = 100
-    SUB_URL = 'http://10.1.0.2/flags'
+    SUB_LIMIT = 1 # number of requests per interval
+    SUB_INTERVAL = 5 # interval duration
+    SUB_PAYLOAD_SIZE = 100 # max flag per request
+    SUB_URL = 'http://10.1.0.2/flags' # flag submission endpoint
 
     SUB_ACCEPTED = 'accepted'
     SUB_INVALID = 'invalid'
